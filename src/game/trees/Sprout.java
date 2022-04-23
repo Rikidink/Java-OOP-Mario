044d626cd4cd1de519342e807832f31df1a9563c
rzha0122 <rzha0122@student.monash.edu>
2022-04-24 02:37:55 +1000

    Added tree growth stages and spawn monsters functionality (not fully)

--- src/game/trees/Sprout.java
+++ src/game/trees/Sprout.java
@@ -1,9 +1,31 @@
 package game.trees;
 
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.Goomba;
+
 public class Sprout extends Tree {
 
     public Sprout(){
         super('+');
         
+    }
+
+    // TODO: Need to figure out how to get mario object into goomba constructor so that goomba moves around
+
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        turnCount += 1;
+
+        if(turnCount == 10){
+            location.setGround(new Sapling());
+        }
+        else {
+            if(Math.random() <= 0.10){
+                if(!location.containsAnActor()){
+                    location.addActor(new Goomba());
+                }
+            }
+        }
     }
 }
--- src/game/trees/Tree.java
+++ src/game/trees/Tree.java
@@ -1,8 +1,11 @@
 package game.trees;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import java.util.Random;
 
 abstract public class Tree extends Ground {
+
+    protected int turnCount = 0;
 
     /**
      * Constructor.
