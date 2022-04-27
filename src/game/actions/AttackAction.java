0e4bf6919c8fde8682120c71f3ee3bd71b14dc11
Rebekah <rful0003@student.monash.edu>
2022-04-27 12:14:45 +1000

    minor code cleanups

--- src/game/actions/AttackAction.java
+++ src/game/actions/AttackAction.java
@@ -49,22 +49,24 @@ public class AttackAction extends Action {
 			return actor + " misses " + target + ".";
 		}
 
-		int damage = weapon.damage();
+		int damage;
+		String result;
 
-		if (actor.hasCapability(Status.HAS_EATEN_POWER_STAR)){ //power stars give one shpt abilities
+		if (actor.hasCapability(Status.HAS_EATEN_POWER_STAR)){ //power stars give one shot abilities
 			damage = 9001;
+		}
+		else {
+			damage = weapon.damage();
 		}
 
 		if (target.hasCapability(Status.HAS_EATEN_POWER_STAR)){ //power stars make all damage 0
 			damage = 0;
 		}
 
-		if (damage != 0 && actor.hasCapability(Status.HAS_EATEN_SUPER_MUSHROOM)) { //getting git for non zero damage removes the super mushroom effect
-			actor.removeCapability(Status.HAS_EATEN_SUPER_MUSHROOM);
+		if (target.hasCapability(Status.TALL)) {
+			target.removeCapability(Status.TALL);
 		}
 		target.hurt(damage);
-
-		String result = ""; //placeholder so INTELLIJ lets me compile the code
 
 		if (!target.isConscious() && target.hasCapability(Status.CAN_BE_DORMANT)) {
 			//if target in going to dormant state...
@@ -72,10 +74,11 @@ public class AttackAction extends Action {
 			target.removeCapability(Status.CAN_BE_DORMANT);
 			result = actor + " " + weapon.verb() + " " + target + ". " + target + " becomes dormant.";
 
-		} else if (target.hasCapability(Status.IS_DORMANT) && !(weapon.verb().equals("wrenches"))){ //trying to attack a dormant actor with ineffective weapon
+		}
+		else if (target.hasCapability(Status.IS_DORMANT) && !(weapon.toString().equals("Wrench"))){ //trying to attack a dormant actor with ineffective weapon
 			result = actor + " " + weapon.verb() + " " + target + ". The dormant " + target + " snickers at the " + actor + " behind his very tough shell.";
-
-		} else { // else actor can be attacked normally
+		}
+		else { // else actor can be attacked normally
 
 			result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
 			if (!target.isConscious()) {
--- src/game/items/Wrench.java
+++ src/game/items/Wrench.java
@@ -8,9 +8,6 @@ import edu.monash.fit2099.engine.weapons.WeaponItem;
 public class Wrench extends WeaponItem {
 
     public Wrench() {
-            super("Wrench", 'W', 50, "wrenches", 80); //the verb is the only way to directly tell what type of weapon item it is so this has to be specific to a wrench
-        }
-
-
-
+            super("Wrench", 'W', 50, "wrenches", 80);
+    }
 }
