d435a37ba7c3659d305d2a0b94543480511a9b0e
Rebekah <rful0003@student.monash.edu>
2022-05-08 20:47:09 +1000

    added fire ground class

---
+++ src/game/ground/FireGround.java
@@ -0,0 +1,32 @@
+package game.ground;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+
+public class FireGround extends Ground {
+    int timeRemaining;
+
+    /**
+     * Constructor.
+     *
+     */
+    public FireGround() {
+        super('f');
+        timeRemaining = 3;
+    }
+
+    @Override
+    public void tick(Location location) {
+        if (timeRemaining == 0) {
+            location.setGround(new Dirt());
+        }
+        else {
+            if (location.containsAnActor()) {
+                Actor actor  = location.getActor();
+                actor.hurt(20);
+            }
+            timeRemaining -= 1;
+        }
+    }
+}
