6acb6daac925e1b9a7f13780429bec1c4f2be989
Rebekah <rful0003@student.monash.edu>
2022-05-18 21:31:24 +1000

    implemented PiranhaPlant attacks player when they approach it

--- src/game/actors/enemies/PiranhaPlant.java
+++ src/game/actors/enemies/PiranhaPlant.java
@@ -3,13 +3,16 @@ package game.actors.enemies;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.Status;
+import game.actions.AttackAction;
 import game.behaviours.AttackBehaviour;
 import game.behaviours.Behaviour;
+import game.behaviours.FollowBehaviour;
 import game.behaviours.GoToFountainBehaviour;
 import game.reset.Resettable;
 
@@ -26,7 +29,30 @@ public class PiranhaPlant extends Enemy implements Resettable {
         registerInstance();
     }
 
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+            actions.add(new AttackAction(this,direction));
+        }
+        return actions;
+    }
 
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        for (Exit exit : map.locationOf(this).getExits()) {
+            if (exit.getDestination().containsAnActor() && exit.getDestination().getActor().hasCapability(Status.HOSTILE_TO_ENEMY)) {
+                behaviours.put(8, new AttackBehaviour(exit.getDestination().getActor()));
+            }
+        }
+
+        for(Behaviour behaviour : behaviours.values()) {
+            Action action = behaviour.getAction(this, map);
+            if (action != null)
+                return action;
+        }
+        return new DoNothingAction();
+    }
 
     @Override
     protected IntrinsicWeapon getIntrinsicWeapon() {
